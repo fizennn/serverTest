9b935fb15f6b83dca913b8e8b3c95cce0cfba3ca
Luu Quang Huy <157577066+fizennn@users.noreply.github.com>
2025-06-27 09:54:40 +0700

    Update upload.service.ts

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -15,7 +15,7 @@ export class UploadService {
   ) {}
 
   generateFileUrl(filename: string): string {
-    const serverLink = 'https://170.64.217.145/v1';
+    const serverLink = '/v1';
     return `${serverLink}/uploads/${filename}`;
   }
 
